f4ce8c00f8371af5a40c28e9ff20f20ca652928d
Ramya2001bca <113408822+Ramya2001bca@users.noreply.github.com>
2022-10-07 18:28:51 +0530

    Add files via upload

---
+++ College3.java
@@ -0,0 +1,20 @@
+public class College3{
+                static String collegename="Idhaya"; //static variable
+                 int staff=5;  //instance variable
+static void principalroom(int awad,String name)   //static method
+                   {
+                                            
+                                            System.out.println(+awad+" /n"+name);
+                                            System.out.println(collegename);
+                    }
+
+ static int add(int a,int b)
+                  {
+                                            
+                      int c=a+b;
+                                            System.out.println(c);
+                      return c;
+                                            }
+                                            }
+
+
---
+++ Game.java
@@ -0,0 +1,12 @@
+public class Game{
+
+
+public static void main(String[] args)
+                        {
+                                                //College3.collegename();
+                                                College3.principalroom(3,"Ramya"); //static method-by accessing class name
+                                                College3 obj=new College3();
+                                                College3.add(5,5);     //static variable 
+
+                            }
+}
